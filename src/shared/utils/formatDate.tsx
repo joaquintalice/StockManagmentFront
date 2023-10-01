f86b13d2ab4f402aef6cd756904ca33281a6ec2b
Joaquin Talice <joaquintalice@gmail.com>
2023-10-01 20:50:22 -0300

    fix: date type fix

--- src/shared/utils/formatDate.tsx
+++ src/shared/utils/formatDate.tsx
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 import "dayjs/locale/es";
 
-const formatDatetime = (date: string) => {
+const formatDatetime = (date: Date) => {
     const formattedDate = dayjs(date).format("DD/MM/YY HH:mm");
     return formattedDate;
 };
