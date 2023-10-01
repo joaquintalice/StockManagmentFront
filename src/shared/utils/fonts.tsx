d71d13b5e8d902f82678936ec890d713f73582ca
Joaquin Talice <joaquintalice@gmail.com>
2023-10-01 20:49:56 -0300

    feat: some google fonts

---
+++ src/shared/utils/fonts.tsx
@@ -0,0 +1,29 @@
+import { Rubik, Oxygen, Roboto, Source_Code_Pro } from 'next/font/google';
+
+export const OxygenFont = Oxygen({
+    weight: '300',
+    style: ['normal'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const RobotoFont = Roboto({
+    weight: '500',
+    style: ['normal'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const RubikFont = Rubik({
+    weight: '900',
+    style: ['normal'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const scFont = Source_Code_Pro({
+    weight: '400',
+    style: 'normal',
+    subsets: ['latin'],
+    display: 'swap'
+})
